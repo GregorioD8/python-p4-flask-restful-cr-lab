812b6c5ea43fc2bd023ec05173c4e27e4afd08fe
Gregorio <Greg.del801@gmail.com>
2024-06-21 10:43:59 -0700

    completed lab

--- server/app.py
+++ server/app.py
@@ -16,11 +16,112 @@ db.init_app(app)
 
 api = Api(app)
 
-class Plants(Resource):
-    pass
+# create a RESTful home page
+class Home(Resource):
 
+    def get(self):
+
+        response_dict = {
+            "message": "Welcome to the Plant RESTful API",
+        }
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+
+api.add_resource(Home, '/')
+
+# Your API should have the following routes as well as the associated controller actions that return the appropriate JSON data:
+
+
+class Plants(Resource):
+
+    # Index Route
+    # GET /plants
+    # Response Body
+    # -------
+    # [
+    #   {
+    #     "id": 1,
+    #     "name": "Aloe",
+    #     "image": "./images/aloe.jpg",
+    #     "price": 11.50
+    #   },
+    #   {
+    #     "id": 2,
+    #     "name": "ZZ Plant",
+    #     "image": "./images/zz-plant.jpg",
+    #     "price": 25.98
+    #   }
+    # ]
+    def get(self):
+
+        plants = [p.to_dict() for p in Plant.query.all()]
+
+        return make_response(jsonify(plants), 200)
+    
+
+    # Create Route
+    # POST /plants
+
+
+    # Headers
+    # -------
+    # Content-Type: application/json
+
+
+    # Request Body
+    # ------
+    # {
+    # "name": "Aloe",
+    # "image": "./images/aloe.jpg",
+    # "price": 11.50
+    # }
+
+
+    # Response Body
+    # -------
+    # {
+    # "id": 1,
+    # "name": "Aloe",
+    # "image": "./images/aloe.jpg",
+    # "price": 11.50
+    # }
+    def post(self):
+        data = request.get_json()
+
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price'],
+        )
+        
+        db.session.add(new_plant)
+        db.session.commit()
+        return make_response(new_plant.to_dict(), 201)
+api.add_resource(Plants, '/plants')
+# Show Route
+# GET /plants/:id
+
+
+# Response Body
+# ------
+# {
+#   "id": 1,
+#   "name": "Aloe",
+#   "image": "./images/aloe.jpg",
+#   "price": 11.50
+# }
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first().to_dict()
+        return make_response(jsonify(plant), 200)
+    
+api.add_resource(PlantByID, '/plants/<int:id>')
+    
         
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -3,7 +3,20 @@ from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
+# Edited the Plant model in models.py to match this specification:
+# Column Name | Data Type
+# name        |
+# image       |
+# price       |
+
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
+
+    def __repr__(self):
+        return f'<Plant {self.name} | Price: {self.price}>'
+    
